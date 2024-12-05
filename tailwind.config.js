9cd496d5c263bc0dc23d4b8fd5f8c8dfbedc2230
kinsonhou <qingshang77@gmail.com>
2024-12-05 01:41:51 -0800

    fix: update tailwind config and package.json for Vercel deployment

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,31 +1,13 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    fontFamily: {
-      display: ['var(--font-display)', 'sans-serif'],
-      body: ['var(--font-body)', 'monospace'],
-    },
     extend: {
-      colors: {
-        brown: {
-          100: '#FFFFFF',
-          200: '#EAD4AA',
-          300: '#E4A672',
-          500: '#B86F50',
-          700: '#743F39',
-          800: '#3F2832',
-          900: '#181425',
-        },
-        clay: {
-          100: '#C0CBDC',
-          300: '#8B9BB4',
-          500: '#5A6988',
-          700: '#3A4466',
-          900: '#181425',
-        },
+      fontFamily: {
+        display: ['"Upheaval Pro"', 'sans-serif'],
+        body: ['"VT323"', 'monospace'],
       },
     },
   },
-  plugins: [require('@tailwindcss/forms')],
+  plugins: [],
 };
