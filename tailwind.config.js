536ec73bfafd04612355f2063f0fa4a03e237dce
kinsonhou <qingshang77@gmail.com>
2024-12-05 01:54:06 -0800

    fix: update tailwind config to use CommonJS for Vercel deployment

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,29 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
       fontFamily: {
         display: ['"Upheaval Pro"', 'sans-serif'],
         body: ['"VT323"', 'monospace'],
+      },
+      colors: {
+        brown: {
+          100: '#FFFFFF',
+          200: '#EAD4AA',
+          300: '#E4A672',
+          500: '#B86F50',
+          700: '#743F39',
+          800: '#3F2832',
+          900: '#181425',
+        },
+        clay: {
+          100: '#C0CBDC',
+          300: '#8B9BB4',
+          500: '#5A6988',
+          700: '#3A4466', // 这是按钮使用的背景色
+          900: '#181425',
+        },
       },
     },
   },
